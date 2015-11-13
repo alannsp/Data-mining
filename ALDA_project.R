30e1197d0854692b6caef56aa4a4f5e655016ec8
kpharasi <kartik.230@gmail.com>
2015-11-13 16:46:20 -0500

    Update ALDA_project.R

--- ALDA_project.R
+++ ALDA_project.R
@@ -60,3 +60,84 @@ for (j in 1:number)
 
 }
 }
+
+
+
+***************************NEW VERSION************************************************
+
+library(gdata)
+help("read.xls")
+Movie_Titles<-read.xls("C:/Users/Kartikeya/Desktop/ALDA Project/movie_titles_small.xls",header= F,perl = "C:/Perl64/bin/perl.exe")
+
+names(Movie_Titles)<- c("Movie_ID","Release_Year","Movie_Title")
+
+setwd("C:/Users/Kartikeya/Desktop/ALDA Project/Dataset/training_set_small/")
+files_list<- list.files()
+
+i<-1
+user_summary<-c()
+for(file in files_list)
+{
+  dataset<-read.table(text=readLines(file)[-1],header=FALSE,sep=",")
+  ratings<-dataset[,c(2)]
+  average<-mean(ratings)
+  standard_deviation<-sd(ratings)
+  number<-length(ratings)
+  
+  #Movie Summary
+  
+  if(!exists("Movie_Summary"))
+  {
+    Movie_Summary<-data.frame(Movie_ID=i,Average_Movie_Rating=average,Standard_Deviation=standard_deviation,Number_of_Ratings=number)
+  }
+  
+  
+  else if(exists("Movie_Summary"))
+  {
+    
+    Movie_Summary_temp<-data.frame(Movie_ID=i,Average_Movie_Rating=average,Standard_Deviation=standard_deviation,Number_of_Ratings=number)
+    Movie_Summary<-rbind(Movie_Summary,Movie_Summary_temp)
+    rm(Movie_Summary_temp)
+  }
+  
+  #User Summary
+  
+  user_id<-dataset[,c(1)]
+  
+  
+  
+  for(j in 1:number)
+  {
+    
+    if(!is.element(user_id[j],user_summary))
+    {
+      release_year<- Movie_Titles[,c(2)]
+      rating_year<- substr(dataset[,c(3)],1,4)
+      difference=strtoi(rating_year[j])-release_year[i]
+      user_summary<-append(user_summary,user_id[j])    
+      assign(paste("User",user_id[j],sep=""),data.frame(Movie_ID=i,Rating=ratings[j],Difference=difference))
+    }
+    
+    else
+    {
+      release_year<- Movie_Titles[,c(2)]
+      rating_year<- substr(dataset[,c(3)],1,4)
+      difference=strtoi(rating_year[j])-release_year[i]
+      user_temp<-data.frame(Movie_ID=i,Rating=ratings[j],Difference=difference)
+      
+      temp<-get(paste("User",user_id[j],sep=""))
+      temp<-rbind(temp,user_temp)
+      assign(paste("User",user_id[j],sep=""),temp)
+      rm(user_temp)
+      rm(temp)
+    }
+    
+  }
+  
+  i<-i+1
+}
+
+(kc<-kmeans(Movie_Summary[,c(2,3,4)],2))
+
+#Release_year_temp<-Movie_Titles[,c[2]]
+#cbind(Movie_Summary,Release_Year= Release_year_temp)
